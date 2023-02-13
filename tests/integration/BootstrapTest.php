17167e15020a211414b1bfd8d9bd511454a7c694
Ricardo Moraleida <moraleida@gmail.com>
2023-02-13 16:43:05 -0300

    introduces proper test infrastructure and first tests

---
+++ tests/integration/BootstrapTest.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace integration;
+
+use StellarWP\Pigeon\Pigeon;
+
+class BootstrapTest extends \Codeception\TestCase\WPTestCase {
+
+	public function test_init_returns_null_if_pigeon_not_enabled() {
+		$instance = Pigeon::init();
+		$this->assertNull( $instance );
+	}
+
+	public function test_is_enabled_returns_false_if_pigeon_not_enabled() {
+		$this->assertFalse( Pigeon::is_enabled() );
+	}
+
+	public function test_is_enabled_returns_true_if_pigeon_enabled() {
+		if ( ! defined( 'STELLARWP_PIGEON_ENABLE' ) ) {
+			define( 'STELLARWP_PIGEON_ENABLE', true );
+		}
+		$this->assertTrue( Pigeon::is_enabled() );
+	}
+
+	public function test_init_returns_instance_if_pigeon_enabled() {
+		if ( ! defined( 'STELLARWP_PIGEON_ENABLE' ) ) {
+			define( 'STELLARWP_PIGEON_ENABLE', true );
+		}
+		$instance = Pigeon::init();
+		$this->assertTrue(  $instance instanceof Pigeon );
+	}
+
+
+}
---
+++ tests/unit/ConfigTest.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace unit;
+
+use StellarWP\Pigeon\Config\Config;
+
+class ConfigTest extends \Codeception\Test\Unit {
+
+	public function test_get_hook_prefix_throws_exception_if_no_prefix() {
+		$this->expectException( \RuntimeException::class );
+		Config::get_hook_prefix();
+	}
+
+	public function test_get_hook_prefix_returns_set_prefix() {
+		Config::set_hook_prefix('prefix');
+		$this->assertEquals( 'prefix', Config::get_hook_prefix() );
+	}
+
+	public function test_set_hook_prefix_throws_exception_if_prefix_set() {
+		$this->expectException( \RuntimeException::class );
+		Config::set_hook_prefix( 'tec' );
+	}
+
+	public function test_reset_removes_set_prefix() {
+		Config::reset();
+		$this->expectException( \RuntimeException::class );
+		Config::get_hook_prefix();
+	}
+
+	/**
+	 * @dataProvider invalid_prefix_provider
+	 */
+	public function test_set_hook_prefix_throws_exception_if_prefix_is_invalid( $prefix ) {
+		$this->expectException( \InvalidArgumentException::class );
+		Config::set_hook_prefix( $prefix );
+	}
+
+	public function invalid_prefix_provider() {
+		return [
+			[ 'pre fix' ],
+			[ 'pre#fix' ],
+			[ 'PreFix' ],
+		];
+	}
+
+}
