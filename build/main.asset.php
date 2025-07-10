977da072c97071a33c94a677fd5cbc922b855a44
Dimitrios Pantazis <dpanta94@gmail.com>
2025-07-10 16:48:56 +0300

    prevent built formatting

--- build/main.asset.php
+++ build/main.asset.php
@@ -1,4 +1,1 @@
-<?php return [
-	'dependencies' => [ 'react', 'react-dom', 'react-jsx-runtime', 'wp-components', 'wp-data', 'wp-date', 'wp-dom-ready', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-primitives' ],
-	'version'      => '5e695a5f99993ed037e8',
-];
+<?php return array('dependencies' => array('react', 'react-dom', 'react-jsx-runtime', 'wp-components', 'wp-data', 'wp-date', 'wp-dom-ready', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-primitives'), 'version' => '5e695a5f99993ed037e8');
