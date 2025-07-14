59dde3c5e9fcdd4733592888e9e740e4fc3594ca
Dimitrios Pantazis <dpanta94@gmail.com>
2025-07-14 16:01:44 +0300

    Delete all orphaned tasks in chunks

--- src/Tasks/Herding.php
+++ src/Tasks/Herding.php
@@ -31,53 +31,61 @@ class Herding extends Task_Abstract {
 	 * @since TBD
 	 */
 	public function process(): void {
-		$task_ids = DB::get_col(
-			DB::prepare(
-				'SELECT DISTINCT(%i) FROM %i WHERE %i NOT IN (SELECT %i FROM %i) LIMIT %d',
-				Tasks::uid_column(),
-				Tasks::table_name(),
-				'action_id',
-				'action_id',
-				DB::prefix( 'actionscheduler_actions' ),
+		DB::beginTransaction();
+
+		/**
+		* Filters the limit of tasks to herd in a single batch.
+		*
+		* @since TBD
+		*
+		* @param int $limit The limit of tasks to herd.
+		*/
+		$batch_size = max( 1, (int) apply_filters( 'shepherd_' . Config::get_hook_prefix() . '_herding_batch_limit', 500 ) );
+
+		do {
+			$task_ids = DB::get_col(
+				DB::prepare(
+					'SELECT DISTINCT(%i) FROM %i WHERE %i NOT IN (SELECT %i FROM %i) LIMIT %d',
+					Tasks::uid_column(),
+					Tasks::table_name(),
+					'action_id',
+					'action_id',
+					DB::prefix( 'actionscheduler_actions' ),
+					$batch_size,
+				)
+			);
+
+			if ( empty( $task_ids ) ) {
 				/**
-				 * Filters the limit of tasks to herd in a single batch.
+				 * Fires when the herding task is processed.
 				 *
 				 * @since TBD
 				 *
-				 * @param int $limit The limit of tasks to herd.
+				 * @param Herding $task The herding task that was processed.
 				 */
-				apply_filters( 'shepherd_' . Config::get_hook_prefix() . '_herding_batch_limit', 500 )
-			)
-		);
+				do_action( 'shepherd_' . Config::get_hook_prefix() . '_herding_processed', $this );
+				return;
+			}
 
-		if ( empty( $task_ids ) ) {
-			/**
-			 * Fires when the herding task is processed.
-			 *
-			 * @since TBD
-			 *
-			 * @param Herding $task The herding task that was processed.
-			 */
-			do_action( 'shepherd_' . Config::get_hook_prefix() . '_herding_processed', $this );
-			return;
-		}
+			$task_ids = implode( ',', array_unique( array_map( 'intval', $task_ids ) ) );
 
-		$task_ids = implode( ',', array_unique( array_map( 'intval', $task_ids ) ) );
+			DB::query(
+				DB::prepare(
+					"DELETE FROM %i WHERE task_id IN ({$task_ids})",
+					Task_Logs::table_name(),
+				)
+			);
 
-		DB::query(
-			DB::prepare(
-				"DELETE FROM %i WHERE task_id IN ({$task_ids})",
-				Task_Logs::table_name(),
-			)
-		);
+			DB::query(
+				DB::prepare(
+					"DELETE FROM %i WHERE %i IN ({$task_ids})",
+					Tasks::table_name(),
+					Tasks::uid_column(),
+				)
+			);
+		} while ( ! empty( $task_ids ) );
 
-		DB::query(
-			DB::prepare(
-				"DELETE FROM %i WHERE %i IN ({$task_ids})",
-				Tasks::table_name(),
-				Tasks::uid_column(),
-			)
-		);
+		DB::commit();
 
 		/**
 		 * Fires when the herding task is processed.
