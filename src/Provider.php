2756df1132fd7f2214dc403cd0732a7d9110275d
Dimitrios Pantazis <dpanta94@gmail.com>
2025-09-07 16:01:34 +0300

    make register_regulator an anonymous function

--- src/Provider.php
+++ src/Provider.php
@@ -81,25 +81,18 @@ class Provider extends Provider_Abstract {
 
 		$prefix = Config::get_hook_prefix();
 
-		add_action( "shepherd_{$prefix}_tables_registered", [ $this, 'register_regulator' ] );
+		add_action(
+			"shepherd_{$prefix}_tables_registered",
+			function() {
+				$this->container->get( Regulator::class )->register();
+			}
+		);
 
 		$this->container->get( Tables_Provider::class )->register();
 
 		add_action( 'action_scheduler_deleted_action', [ $this, 'delete_tasks_on_action_deletion' ] );
 
 		self::$has_registered = true;
-	}
-
-	/**
-	 * Registers the regulator when its safe to do so
-	 * since the tables have been created/updated successfully.
-	 *
-	 * @since 0.0.7
-	 *
-	 * @return void
-	 */
-	private function register_regulator(): void {
-		$this->container->get( Regulator::class )->register();
 	}
 
 	/**
